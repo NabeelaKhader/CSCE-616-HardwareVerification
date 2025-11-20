f60766e2a30140a9df2052190b9c22048c7243aa
NabeelaKhader <nabeela.khader09@gmail.com>
2025-11-19 19:42:15 -0600

    Delete work directory

--- work/sim/xcelium.d/run.lnx86.22.03.d/files.ts
+++
@@ -1,2 +0,0 @@
-1724712212 /home/grads/n/nabeelakhader/lab-0-NabeelaKhader/work/design/common/addr_segregator_proc.sv
-1724720511 /home/grads/n/nabeelakhader/lab-0-NabeelaKhader/work/tb/tb_addr_segregator_proc.sv
